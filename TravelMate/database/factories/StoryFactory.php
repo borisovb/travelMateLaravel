c8a4dc9b396a199b9a094106c55db586e805e17b
Hadzhiev,Petar P.A <364609@strudent.fontys.nl>
2018-11-27 20:00:01 +0100

    Database populated via seeder

--- TravelMate/database/factories/StoryFactory.php
+++ TravelMate/database/factories/StoryFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Story::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence,
+        'content' => $faker->realText(200)
     ];
 });
